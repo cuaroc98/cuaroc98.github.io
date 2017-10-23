c074c4db4e06966505aed0c246850e2464b068a6
cuaroc98 <p.kothmair@gmail.com>
2017-10-23 18:04:41 +0200

    a

--- js/functions.js
+++ js/functions.js
@@ -1,37 +1,35 @@
-<script>
-	var ids = ["contact", "about", "times"];
-	
-	function showTest(id)
+var ids = ["contact", "about", "times"];
+
+function showTest(id)
+{
+	jQuery('#' + id).slideToggle();
+	jQuery('#' + id).attr('active', 'true');
+	document.getElementById(id + '-btn').onclick = function(){hide(id)};
+}
+
+function show(id)
+{
+	for(var i = 0; i < ids.length; i++)
 	{
-		jQuery('#' + id).slideToggle();
-		jQuery('#' + id).attr('active', 'true');
-		document.getElementById(id + '-btn').onclick = function(){hide(id)};
-	}
-	
-	function show(id)
-	{
-		for(var i = 0; i < ids.length; i++)
+		if(id != ids[i])
 		{
-			if(id != ids[i])
-			{
-				hide(ids[i]);
-			}
-			else if(jQuery('#' + id).attr('active') == 'false')
-			{
-				jQuery('#' + id).slideToggle();
-				jQuery('#' + id).attr('active', 'true');
-				document.getElementById(id + '-btn').onclick = function(){hide(id)};
-			}
+			hide(ids[i]);
 		}
-	}
-	
-	function hide(id)
-	{
-		if(jQuery('#' + id).attr('active') == 'true')
+		else if(jQuery('#' + id).attr('active') == 'false')
 		{
 			jQuery('#' + id).slideToggle();
-			jQuery('#' + id).attr('active', 'false');
-			document.getElementById(id + "-btn").onclick = function(){show(id)};
+			jQuery('#' + id).attr('active', 'true');
+			document.getElementById(id + '-btn').onclick = function(){hide(id)};
 		}
 	}
-</script>
+}
+
+function hide(id)
+{
+	if(jQuery('#' + id).attr('active') == 'true')
+	{
+		jQuery('#' + id).slideToggle();
+		jQuery('#' + id).attr('active', 'false');
+		document.getElementById(id + "-btn").onclick = function(){show(id)};
+	}
+}
