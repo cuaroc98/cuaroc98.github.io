3b071c45cc4e691f1b5589d3a9a1a51602ae408b
cuaroc98 <p.kothmair@gmail.com>
2017-10-23 18:04:03 +0200

    create functions.js

---
+++ js/functions.js
@@ -0,0 +1,37 @@
+<script>
+	var ids = ["contact", "about", "times"];
+	
+	function showTest(id)
+	{
+		jQuery('#' + id).slideToggle();
+		jQuery('#' + id).attr('active', 'true');
+		document.getElementById(id + '-btn').onclick = function(){hide(id)};
+	}
+	
+	function show(id)
+	{
+		for(var i = 0; i < ids.length; i++)
+		{
+			if(id != ids[i])
+			{
+				hide(ids[i]);
+			}
+			else if(jQuery('#' + id).attr('active') == 'false')
+			{
+				jQuery('#' + id).slideToggle();
+				jQuery('#' + id).attr('active', 'true');
+				document.getElementById(id + '-btn').onclick = function(){hide(id)};
+			}
+		}
+	}
+	
+	function hide(id)
+	{
+		if(jQuery('#' + id).attr('active') == 'true')
+		{
+			jQuery('#' + id).slideToggle();
+			jQuery('#' + id).attr('active', 'false');
+			document.getElementById(id + "-btn").onclick = function(){show(id)};
+		}
+	}
+</script>
