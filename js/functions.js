6f99951cef01932278dbe05a7f91cb8885333a38
cuaroc98 <p.kothmair@gmail.com>
2017-10-24 22:07:43 +0200

    ss

--- js/functions.js
+++ js/functions.js
@@ -37,24 +37,25 @@ function resize(img_id)
 {
 	jQuery('#' + img_id).toggleClass('small large');
 	jQuery('#' + img_id + '-cont').toggleClass('small-cont large-cont');
+	jQuery('#' + img_id + '-story').toggleClass('small-story large-story');
 }
 
 function initMaps()
-	{
-		initMap(51.50694616, 7.46843398, 13, 'swim-map');
-		//initMap(51.50694616, 7.46843398, 13, 'bike-map');
-		initMap(51.49331465, 7.45424509, 14, 'run-map');
-	}
-	
-	function initMap(lat, lng, zoom, element_id)
-	{
-		var pos = {lat: lat, lng:lng};
-		var map = new google.maps.Map(document.getElementById(element_id), {
-			zoom: zoom,
-			center: pos
-		});
-		var mark = new google.maps.Marker({
-          position: pos,
-          map: map
-        });
-	}
+{
+	initMap(51.50694616, 7.46843398, 13, 'swim-map');
+	//initMap(51.50694616, 7.46843398, 13, 'bike-map');
+	initMap(51.49331465, 7.45424509, 14, 'run-map');
+}
+
+function initMap(lat, lng, zoom, element_id)
+{
+	var pos = {lat: lat, lng:lng};
+	var map = new google.maps.Map(document.getElementById(element_id), {
+		zoom: zoom,
+		center: pos
+	});
+	var mark = new google.maps.Marker({
+	  position: pos,
+	  map: map
+	});
+}
