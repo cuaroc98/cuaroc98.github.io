504c447b3dbc496900859f8c1a42e3a18c74636e
cuaroc98 <p.kothmair@gmail.com>
2017-10-24 21:49:52 +0200

    ssss

--- js/functions.js
+++ js/functions.js
@@ -38,3 +38,23 @@ function resize(img_id)
 	jQuery('#' + img_id).toggleClass('small large');
 	jQuery('#' + img_id + '-cont').toggleClass('small-cont large-cont');
 }
+
+function initMaps()
+	{
+		initMap(51.50694616, 7.46843398, 13, 'swim-map');
+		//initMap(51.50694616, 7.46843398, 13, 'bike-map');
+		initMap(51.49331465, 7.45424509, 14, 'run-map');
+	}
+	
+	function initMap(lat, lng, zoom, element_id)
+	{
+		var pos = {lat: lat, lng:lng};
+		var map = new google.maps.Map(document.getElementById(element_id), {
+			zoom: zoom,
+			center: pos
+		});
+		var mark = new google.maps.Marker({
+          position: pos,
+          map: map
+        });
+	}
