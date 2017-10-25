7be7c0d42d5118b3486ebd2ddd47eed2063c11c9
cuaroc98 <p.kothmair@gmail.com>
2017-10-25 17:22:51 +0200

    sss

--- js/functions.js
+++ js/functions.js
@@ -40,33 +40,55 @@ function resize(img_id)
 {
 	jQuery('#' + img_id).toggleClass('small large');
 	jQuery('#' + img_id + '-cont').toggleClass('small-cont large-cont');
-	//jQuery('#' + img_id + '-story').slideToggle(600);
 	jQuery('#' + img_id + '-story').toggleClass('small-story large-story');
 	jQuery('#' + img_id + '-details').slideToggle(1000);
-	if(img_id = 'swim-pic' && swim == 0) {initMap(51.50694616, 7.46843398, 13, 'swim-map'); swim = 1;}
+	jQuery('#' + img_id + '-border').toggleClass('small-border large-border');
+	if(img_id = 'swim-pic' && swim == 0) {initSwimMap(); swim = 1;}
 	if(img_id = 'swim-pic' && swim == 1) swim = 0;
-	if(img_id = 'bike-pic' && bike == 0) {initMap(51.50694616, 7.46843398, 13, 'bike-map'); bike = 1;}
+	if(img_id = 'bike-pic' && bike == 0) {initBikeMap(); bike = 1;}
 	if(img_id = 'bike-pic' && bike == 1) bike = 0;
-	if(img_id = 'run-pic' && run == 0) {initMap(51.49331465, 7.45424509, 14, 'run-map'); run = 1;}
+	if(img_id = 'run-pic' && run == 0) {initRunMap(); run = 1;}
 	if(img_id = 'run-pic' && run == 1) run = 0;
 }
 
+//MAPS
 function initMaps()
 {
-	initMap(51.50694616, 7.46843398, 13, 'swim-map');
-	initMap(51.50694616, 7.46843398, 13, 'bike-map');
-	initMap(51.49331465, 7.45424509, 14, 'run-map');
+	initSwimMap();
+	initBikeMap();
+	initRunMap();
 }
 
-function initMap(lat, lng, zoom, element_id)
+function initSwimMap()
+{
+	initMap(51.5075000, 7.4430556, 12, 'swim-map', [{pos: {lat: 51.50694616, lng:7.46843398}, label: 'A'}, {pos: {lat: 51.508187, lng: 7.417848}, label: 'B'}]);
+}
+
+function initBikeMap()
+{
+	initMap(51.50694616, 7.46843398, 13, 'bike-map', [{pos: {lat: 51.50694616, lng: 7.46843398}, label: 'C'}]);
+}
+
+function initRunMap()
+{
+	initMap(51.49331465, 7.45424509, 13, 'run-map', [{pos: {lat: 51.49331465, lng: 7.45424509}, label: 'E'}]);
+}
+
+function initMap(lat, lng, zoom, element_id, markers)
 {
 	var pos = {lat: lat, lng:lng};
 	var map = new google.maps.Map(document.getElementById(element_id), {
 		zoom: zoom,
 		center: pos
 	});
-	var mark = new google.maps.Marker({
-	  position: pos,
-	  map: map
-	});
+	
+	for(var i = 0; i < markers.length; i++)
+	{
+		marker = markers[i];
+		var mark = new google.maps.Marker({
+	    position: marker.pos,
+	    map: map,
+	    label: marker.label
+		});
+	}
 }
